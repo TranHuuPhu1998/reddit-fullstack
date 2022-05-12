84e6cee4b847d74eac7222acf0d7118e11ba6d32
phutran <phutran@phutrans-MacBook-Pro.local>
2022-05-13 00:03:50 +0700

    init project

---
+++ server/src/index.ts
@@ -0,0 +1,25 @@
+require("dotenv").config();
+import "reflect-metadata";
+import express from "express";
+import { createConnection } from "typeorm";
+import { User } from "./entities/User";
+
+const main = async () => {
+  await createConnection({
+    type: "postgres",
+    database: "reddit",
+    username: process.env.DB_USERNAME_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    logging: true,
+    synchronize: true,
+    entities: [User],
+  });
+
+  const app = express();
+
+  app.listen(4000, () => {
+    console.log(`Server started on port 4000`);
+  });
+};
+
+main().catch((err) => console.log("err", err));
